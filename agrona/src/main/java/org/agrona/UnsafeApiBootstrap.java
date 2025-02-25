4acff690bc3b01ed848185d8a3853d3b1ee41a88
Peter Veentjer <alarmnummer@gmail.com>
2025-02-25 14:46:24 +0200

    licence cleanup

--- agrona/src/main/java/org/agrona/UnsafeApiBootstrap.java
+++ agrona/src/main/java/org/agrona/UnsafeApiBootstrap.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2014-2025 Real Logic Limited.
- * Copyright 2012 The Netty Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
