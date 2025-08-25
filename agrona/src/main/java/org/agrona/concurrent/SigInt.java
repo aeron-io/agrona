c62be1601914bdf6210b2d662157bce73afb259f
Dmytro Vyazelenko <696855+vyazelenko@users.noreply.github.com>
2025-08-25 15:40:04 +0200

    [Java] Invoke original signal handler for `SIGINT/SIGTERM` signals that were previously overridden by `ShutdownSignalBarrier` or via `org.agrona.concurrent.SigInt.register(java.lang.Runnable)` method call. This allows JVM to execute shutdown hooks registered via `java.lang.Runtime.addShutdownHook` method.

--- agrona/src/main/java/org/agrona/concurrent/SigInt.java
+++ agrona/src/main/java/org/agrona/concurrent/SigInt.java
@@ -16,6 +16,7 @@
 package org.agrona.concurrent;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Objects;
@@ -28,8 +29,10 @@ public final class SigInt
 {
     private static final Class<?> SIGNAL_HANDLER_CLASS;
     private static final Constructor<?> SIGNAL_CONSTRUCTOR;
-    private static final Method HANDLE_METHOD;
-    private static final Method RAISE_METHOD;
+    private static final Method SIGNAL_HANDLE_METHOD;
+    private static final Method SIGNAL_RAISE_METHOD;
+    private static final Method HANDLER_HANDLE_METHOD;
+    static final Object NO_OP_SIGNAL_HANDLER;
 
     static
     {
@@ -38,8 +41,27 @@ public final class SigInt
             final Class<?> signalClass = Class.forName("jdk.internal.misc.Signal");
             SIGNAL_HANDLER_CLASS = Class.forName("jdk.internal.misc.Signal$Handler");
             SIGNAL_CONSTRUCTOR = signalClass.getConstructor(String.class);
-            HANDLE_METHOD = signalClass.getMethod("handle", signalClass, SIGNAL_HANDLER_CLASS);
-            RAISE_METHOD = signalClass.getMethod("raise", signalClass);
+            SIGNAL_HANDLE_METHOD = signalClass.getMethod("handle", signalClass, SIGNAL_HANDLER_CLASS);
+            SIGNAL_RAISE_METHOD = signalClass.getMethod("raise", signalClass);
+            HANDLER_HANDLE_METHOD = SIGNAL_HANDLER_CLASS.getMethod("handle", signalClass);
+
+            NO_OP_SIGNAL_HANDLER = Proxy.newProxyInstance(
+                SIGNAL_HANDLER_CLASS.getClassLoader(),
+                new Class<?>[]{ SIGNAL_HANDLER_CLASS },
+                (proxy, method, args) ->
+                {
+                    if (Object.class == method.getDeclaringClass())
+                    {
+                        return switch (method.getName())
+                        {
+                            case "toString" -> proxy.toString();
+                            case "hashCode" -> proxy.hashCode();
+                            case "equals" -> proxy.equals(args[0]);
+                            default -> null;
+                        };
+                    }
+                    return null;
+                });
         }
         catch (final ReflectiveOperationException e)
         {
@@ -64,26 +86,68 @@ public final class SigInt
         try
         {
             final Object signal = SIGNAL_CONSTRUCTOR.newInstance(name);
+            final Object prevHandler = SIGNAL_HANDLE_METHOD.invoke(null, signal, NO_OP_SIGNAL_HANDLER);
+
             final Object handler = Proxy.newProxyInstance(
                 SIGNAL_HANDLER_CLASS.getClassLoader(),
                 new Class<?>[]{ SIGNAL_HANDLER_CLASS },
                 (proxy, method, args) ->
                 {
-                    if (SIGNAL_HANDLER_CLASS == method.getDeclaringClass())
+                    if (SIGNAL_HANDLER_CLASS == method.getDeclaringClass() && HANDLER_HANDLE_METHOD.equals(method))
                     {
-                        task.run();
+                        Throwable error = null;
+                        try
+                        {
+                            task.run();
+                        }
+                        catch (final Throwable ex)
+                        {
+                            error = ex;
+                        }
+                        finally
+                        {
+                            if (null != prevHandler && NO_OP_SIGNAL_HANDLER != prevHandler)
+                            {
+                                try
+                                {
+                                    HANDLER_HANDLE_METHOD.invoke(prevHandler, args[0]);
+                                }
+                                catch (final InvocationTargetException ex)
+                                {
+                                    final Throwable cause = ex.getCause();
+                                    if (null == error)
+                                    {
+                                        error = cause;
+                                    }
+                                    else
+                                    {
+                                        error.addSuppressed(cause);
+                                    }
+                                }
+                            }
+                        }
+
+                        if (null != error)
+                        {
+                            throw error;
+                        }
                     }
                     else if (Object.class == method.getDeclaringClass())
                     {
-                        if (method.getName().equals("toString"))
+                        return switch (method.getName())
                         {
-                            return args[0].toString();
-                        }
+                            case "toString" -> proxy.toString();
+                            case "hashCode" -> proxy.hashCode();
+                            case "equals" -> proxy.equals(args[0]);
+                            default -> null;
+                        };
                     }
                     return null;
                 });
 
-            return HANDLE_METHOD.invoke(null, signal, handler);
+            SIGNAL_HANDLE_METHOD.invoke(null, signal, handler);
+
+            return prevHandler;
         }
         catch (final ReflectiveOperationException e)
         {
@@ -91,9 +155,14 @@ public final class SigInt
         }
     }
 
+    static Object installHandler(final String name, final Object handler) throws ReflectiveOperationException
+    {
+        return SIGNAL_HANDLE_METHOD.invoke(null, SIGNAL_CONSTRUCTOR.newInstance(name), handler);
+    }
+
     static Object raiseSignal(final String name) throws ReflectiveOperationException
     {
-        return RAISE_METHOD.invoke(null, SIGNAL_CONSTRUCTOR.newInstance(name));
+        return SIGNAL_RAISE_METHOD.invoke(null, SIGNAL_CONSTRUCTOR.newInstance(name));
     }
 
     private SigInt()
