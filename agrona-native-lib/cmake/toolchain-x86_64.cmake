194a40fd43837df229027b6cf309031a767e6f0d
EmilJohn24 <emil.lopez@weareadaptive.com>
2026-09-23 08:42:47 +0800

    Switch compiler for all toolchains to Clang to make multi-arch an easier setup.

--- agrona-native-lib/cmake/toolchain-x86_64.cmake
+++ agrona-native-lib/cmake/toolchain-x86_64.cmake
@@ -1,3 +1,4 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
-set(CMAKE_C_COMPILER x86_64-linux-gnu-gcc)
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_COMPILER_TARGET x86_64-linux-gnu)
